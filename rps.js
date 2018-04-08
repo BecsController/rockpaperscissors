6f9272bc8d39c9cc67183518cad221123b56b994
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-04-08 15:05:16 +1200

    playRound fix

--- rps.js
+++ rps.js
@@ -1,77 +1,76 @@
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener('DOMContentLoaded', start);
 
 function start() {
 
   var rockButton = document.getElementsByClassName("rock")[0];
   var paperButton = document.getElementsByClassName("paper")[0];
   var scissorsButton = document.getElementsByClassName("scissors")[0];
+  let computerSelection = "rock";
+  let playerSelection = "rock";
 
   rockButton.addEventListener("click", function() {
-  let playerSelection = 'rock';
-  computerPlay()
+  let playerSelection = "rock";
+  alert("Player Chooses " + playerSelection);
+  computerPlay();
 });
   paperButton.addEventListener("click", function() {
-  let playerSelection = 'paper';
-  computerPlay()
+  let playerSelection = "paper";
+  alert("Player Chooses " + playerSelection);
+  computerPlay();
 });
   scissorsButton.addEventListener("click", function() {
-  let playerSelection = 'scissors';
-  computerPlay()
+  let playerSelection = "scissors";
+  alert("Player Chooses " + playerSelection);
+  computerPlay();
 });
 
-}
 
 let options = ["rock", "paper", "scissors"];
+let roundCountComp = 0;
+let roundCountPlayer = 0;
+let result = document.getElementById("result");
 
   function computerPlay (){
     let arrayPosition = Math.floor(Math.random()*2);
     let computerSelection = options[arrayPosition];
     alert("Computer plays " + computerSelection);
-    playRound()
+    playRound(playerSelection, computerSelection)
   }
 
-let result = getElementById("result").innerHTML;
+  function playRound (playerSelection, computerSelection){
 
-  function playRound(playerSelection, computerSelection){
    switch(playerSelection){
-     case 'rock':
-     if (computerSelection === 'paper'){
-       result = 'Paper covers rock!  One for the Comp!';
-       result.classList.toggle('invisible');
+     case "rock":
+     if (computerSelection === "paper"){
+       result.innerHTML = 'Paper covers rock!  One for the Comp!';
        roundCountComp++;
-     } else if (computerSelection === 'scissors'){
-       result = 'Rock breaks scissors!  One for the challenger!';
-       result.classList.toggle('invisible');
+     } else if (computerSelection === "scissors"){
+       result.innerHTML ='Rock breaks scissors!  One for the challenger!';
        roundCountPlayer++;
      } else {
-       result = 'Tied!';
-       result.classList.toggle('invisible');
+       result.innerHTML ='Tied!';
      } break;
-     case 'paper':
-     if (computerSelection === 'scissors'){
-       result = 'Scissors cuts paper! One for the Comp!';
-       result.classList.toggle('invisible');
+     case "paper":
+     if (computerSelection === "scissors"){
+       result.innerHTML = 'Scissors cuts paper! One for the Comp!';
        roundCountComp++;
-     }else if (computerSelection === 'rock'){
-       result = 'Paper covers rock!  One for the challenger!';
-       result.classList.toggle('invisible');
+     }else if (computerSelection === "rock"){
+       result.innerHTML = 'Paper covers rock!  One for the challenger!';
        roundCountPlayer++;
      } else{
-       result = 'A draw!';
-       result.classList.toggle('invisible');
+       result.innerHTML = 'A draw!';
      } break;
-     case 'scissors':
-     if (computerSelection === 'rock'){
-       result = 'Rock breaks scissors!  One for the comp!';
-       result.classList.toggle('invisible');
+     case "scissors":
+     if (computerSelection === "rock"){
+       result.innerHTML = 'Rock breaks scissors!  One for the comp!';
        roundCountComp++;
-     } else if (computerSelection === 'paper'){
-       result = 'Scissors cut paper!  One for the challenger!';
-       result.classList.toggle('invisible');
+     } else if (computerSelection === "paper"){
+       result.innerHTML = 'Scissors cut paper!  One for the challenger!';
        roundCountPlayer++;
      } else{
-       result = 'No winner today';
-       result.classList.toggle('invisible');
+       result.innerHTML = 'No winner today';
      }
    }
+
   }
+}
